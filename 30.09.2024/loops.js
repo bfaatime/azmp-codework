ded5dcbce95b858b0d18d7d2834c399f44befbb1
bfaatime <fatimebexseliyeva@icloud.com>
2024-10-30 19:22:19 +0400

    added

---
+++ 30.09.2024/loops.js
@@ -0,0 +1,126 @@
+// for (let index = 0; index < 100; index++) {
+//     if (index %2 == 0) {
+//         console.log(index)
+//     }
+    
+// }
+// *********************************************
+// for(let i = 0; i <= 100; i++) {
+//     if (i%2!=0) {
+//         count++
+//     }
+// }
+// ***********************************************
+// console.log(count)
+// let sum = 0;
+// for (let index = 1; index < 101; index++) {
+//     if(index % 5 == 0) {
+//         sum += index = 0
+//     }
+// }
+// console.log(sum);
+// *************************************************************************************************************************************
+// let arr=[12,14,35,56,68,93];
+// let sum = 0;
+// for (let i = 0; i < arrlength; arr++) {
+//    sum += arr[i]
+// }
+// console.log(sum)
+// ********************************************************
+// let arr=[12,14,35,56,68,93];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     if (i %2==0) {
+//     console.log(arr[i])
+//     
+//     }
+// }
+// console.log(sum)
+// **************************************************************
+// let arr=[12,14,35,56,68,93];
+// let largetsNum=arr{0};
+// sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > largestNum) {
+//         largestNum= arr[i]
+//     }
+    
+// }console.log(sum)
+// // ****************************************************************
+// let arr=[12,14,35,56,68,93];
+// let point=55;
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i]=point) {
+//         console.log("bu eded yoxdur")
+//     }  
+// }
+// ******************************************************************************************************************
+// const employees = [
+//     { name: "Javid", salary: 50000, department: "IT" },
+//     { name: "Fatime", salary: 60000, department: "HR" },
+//     { name: "Efsane", salary: 55000, department: "IT" },
+//     { name: "Jabbar", salary: 75000, department: "HR" },
+//     { name: "Meleyke", salary: 65000, department: "IT" },
+//     { name: "Hummet", salary: 80000, department: "HR" },
+//     { name: "Ramil", salary: 70000, department: "IT" },
+//   ];
+//   for (let index = 0; index <  employees .length; index++) {
+//     if ( employees[index].salary>6000) {
+//         console.log( employees [index].name);
+        
+//     }
+    
+//   }
+// // *******************************************************************************************
+// let number=+prompt ("eded daxil edin")
+// let faktorial=1;
+// for (let i = 1; i <= number; i++) {
+//      faktorial*=i;
+// }
+// alert(faktorial)
+// let str=+"lorem ipsum dolar sit amet consectetur adipising elit.";
+// let c = 0;
+
+// for (let i = 0; i < str.length; i++) {
+//    if(str[i].toLowerCase()==="1"){
+//     c++
+//    }
+    
+// }
+// console.log("count of 1:",c)
+// const countries = ["azerbaijan", "norwey", "albania", "germany", "america"];
+
+// for (let i = 0; i < countries.length; i++) {
+//     if(countries[i][0]==="a" && countries[i][countries[i].length - 1] === "a"){
+//         console.log(countries[i]);
+        
+//     }
+    
+// }
+// const countries = ["azerbaijan", "norwey", "albania", "germany", "america"]; 
+
+// let eded=+prompt ("eded daxil edin");
+// let number;
+// for (let i = 2; i<eded / 2; i++) {
+//     if (n % i === 0) {
+        
+//     }
+// alert("eded sadedir")
+    
+// } alert("eded sade deyil")
+// ***********************************************************************************************************************************
+// let arr= [60, 79, 88, 75, 93, 45]
+// let sum=0;
+// for (let index = 0; index < arr.length; index++) {
+//   sum+=arr[index]
+// }
+// console.log(sum);
+
+
+let arr= [60, 79, 88, 75, 93, 45]
+let sum=0;
+for (let index = 0; index < arr.length; index++) {
+  sum+=arr[index] / arr.length
+}
+console.log(sum);
+
